b2b29873a1a740ed70f0ae14f29d218017be2da7
Rodrigo Lima Rodrigues <rodrigaolima11@gmail.com>
2025-05-13 21:34:00 -0300

    commit

--- class16/index.js
+++ class16/index.js
@@ -1,11 +1,24 @@
-const temp = 20;
 
-if(temp > 0){
-    console.log('The weather is GOOD');
-}
-else if(temp <= 30){
+// AND = &&
+// OR = ||
+// NOT = !
+
+
+const temp = 100;
+
+if(temp > 0 && temp <= 30){
     console.log('The weather is GOOD');
 }
 else{
     console.log('The weather is BAD');
 }
+
+const isSunny = true;
+
+if(!isSunny){
+    console.log('It is CLOUDY!');
+}
+else{
+    console.log('It is SUNNY!')
+}
+
